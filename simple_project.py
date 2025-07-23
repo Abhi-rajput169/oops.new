ca184ef4505ce57467fb9d57d20b2f593bc46b23
Abhishek Rajput <rajputabhishek0108@gmail.com>
2025-07-23 18:26:08 +0530

    third

--- simple_project.py
+++ simple_project.py
@@ -11,16 +11,17 @@ class smallapp:
                          2.click 2 to sign in
                          3.click 3 t0 write a post
                          4.click 4 to message a friend
-                         5.clcik any key to exit""")
+                         5.clcik any key to exit
+                         ->""")
         
         if user_input=="1":
             self.signup()
         elif user_input=="2":
             self.signin()
         elif user_input=="3":
-            pass
+            self.postwriting()
         elif user_input=="4":
-            pass
+            self.messagefriend()
         else:
             exit()
 
@@ -44,7 +45,23 @@ class smallapp:
                 print("please input correct information")
         self.menu()
 
-    # def postwriting(self):
-    #     post=input("enter your post")
+    def postwriting(self):
+        if self.loggedin==True:
+            post=input("enter about your post")
+            print(f"this is the content->{post}")
+        else:
+            print("please sign in first")
+        
+        self.menu()
+    
+    def messagefriend(self):
+        if self.loggedin==True:
+            msg=input("enter your msg")
+            print(f"message send to friend->{msg}")
+        else:
+            print("please loggin first")
+
+        self.menu()
+        
 
 abhi=smallapp()
